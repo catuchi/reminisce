6e33196a116f469d002c3d0d5a25d618729628ef
Chibu <atuchibueze@gmail.com>
2022-10-10 20:13:34 -0400

    add route controllers

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://catuchi:developer@cluster0.lg8veau.mongodb.net/reminisce?retryWrites=true&w=majority";
+  "mongodb+srv://catuchi:developer@cluster0.nmhtfxw.mongodb.net/reminisce?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5001;
 
