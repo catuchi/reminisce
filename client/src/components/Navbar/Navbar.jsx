6f9100324f7b0660e932c09a64608ebe64df71bb
Chibu <atuchibueze@gmail.com>
2022-10-12 12:58:35 -0400

    implement google logout

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { AppBar, Avatar, Toolbar, Typography, Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import memories from "../../images/memories.png";
 import useStyles from "./styles";
 
 const Navbar = () => {
   const classes = useStyles();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
 
-  console.log(user);
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
 
-  // useEffect(() => {
-  //   const token = user?.token;
+    history.push("/");
 
-  //   // JWT ...
+    setUser(null);
+  };
 
-  //   setUser(JSON.parse(localStorage.getItem("profile")));
-  // });
+  useEffect(() => {
+    const token = user?.token;
+
+    // JWT ...
+
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -54,6 +64,7 @@ const Navbar = () => {
               variant="contained"
               className={classes.logout}
               color="secondary"
+              onClick={logout}
             >
               Logout
             </Button>
