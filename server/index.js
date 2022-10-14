1447184596f7e17a858136895aac1efc6b1fd164
Chibu <atuchibueze@gmail.com>
2022-10-14 13:43:01 -0400

    make it better

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 import express from "express";
 import bodyParser from "body-parser";
